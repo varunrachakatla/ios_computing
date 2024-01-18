6b294156ed6a99fb891278cf2fa59b4645ac10d9
Varunraj Rachakatla <42968040+varunrachakatla@users.noreply.github.com>
2024-01-18 10:19:30 -0600

    my first commit

---
+++ RachakatlaPlayground.playground/Contents.swift
@@ -0,0 +1,75 @@
+import UIKit
+
+var greeting = "Hello, playground"
+print("Hello, World!");
+var pl = "Swift";
+print("My favorite programming language is \(pl)");
+
+
+
+print("Hello!",10,17.6)
+
+var age: Int = 26
+
+print("you are \(age) years old and in another \(age) years, you will be \(age*2) 😆")
+
+
+print("""
+Hello
+World!
+Swift is Awesome
+""")
+
+let MyString : String = "Hello World"
+print(MyString, "😇")
+
+
+print("Welcome to Swift Programming")
+print("Fall 2021")
+print("*****************")
+print("Welcome to Swift Programming", terminator: "-")
+print("Fall 2021")
+
+
+print ("The list of numbers are ")
+print(1,2,3,4,5,6)
+print("The new pattern is")
+print(1,2,3,4,5,6,separator: "-")
+
+
+
+var mobileBRand = "Apple";
+
+mobileBRand = "Samsung"
+
+print(mobileBRand)
+
+
+let pi = 3.14
+
+print(pi)
+
+var ageNew: Int = 23
+
+ageNew = ageNew*2
+
+print(age)
+
+
+var aweMessage = "This is Superb!"
+
+print(aweMessage)
+
+print("aweMessage")
+
+var course1 = "IOS"
+var course2 = "JAVA"
+
+print(course1,course2)
+
+print(course1,"-",course2)
+
+
+print(10,20,30)
+
+print(12.5,15.5)
